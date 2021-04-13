0fe7190b1a258eff797390d8961c6fc19fd373a0
HadIhab <chocovani101@hotmail.fr>
2021-04-13 14:23:05 +0100

    Providing props

--- src/main/index.js
+++ src/main/index.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
       return (
         <div className="container">
-          <Header />
+          <Header subtitle="Providing houses across the country"/>
         </div>
       );
     }
