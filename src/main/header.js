31a68bc2c6498573a96526d9bc3f6bfdaffd6c30
HadIhab <chocovani101@hotmail.fr>
2021-04-13 14:08:35 +0100

    Creating a Simple Component

---
+++ src/main/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+		<header className="row">
+			<div className="col-md-5">
+			</div>
+			<div className="col-md-7 mt-5 subtitle">
+				Providing houses across the country 
+			</div>
+		</header>
+	);
